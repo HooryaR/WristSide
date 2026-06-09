9d86ddbbcb7794edcabecc64bfdc3d5620a60ae4
Hoorya Rafiq <rafiqho@sheridancollege.ca>
2026-06-09 14:30:27 -0400

    Initial Commit

---
+++ WristSide Watch App/WristSideApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristSideApp.swift
+//  WristSide Watch App
+//
+//  Created by Hoorya Rafiq on 2026-06-09.
+//
+
+import SwiftUI
+
+@main
+struct WristSide_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WristSide Watch AppTests/WristSide_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WristSide_Watch_AppTests.swift
+//  WristSide Watch AppTests
+//
+//  Created by Hoorya Rafiq on 2026-06-09.
+//
+
+import Testing
+@testable import WristSide_Watch_App
+
+struct WristSide_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

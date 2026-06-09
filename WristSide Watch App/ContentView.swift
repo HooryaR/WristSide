3ba7791181e3e53c2134ac6c21905e44ff904ea8
Hoorya Rafiq <rafiqho@sheridancollege.ca>
2026-06-09 15:00:29 -0400

    Implemented initial UI

--- WristSide Watch App/ContentView.swift
+++ WristSide Watch App/ContentView.swift
@@ -10,10 +10,50 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("9:41")
+                .bold()
+                .font(Font.system(.largeTitle, design: .rounded))
+            
+            HStack {
+                Text("NYK 2")
+                Text("-")
+                Text("1 SAS")
+            }.foregroundStyle(.gray)
+            
+            Text("Finals")
+                .font(Font.system(.footnote, design: .rounded))
+                .foregroundStyle(.gray)
+            
+            Spacer()
+            
+            Text("Wemby 4 blocks - Knicks avoiding the paint")
+                .font(Font.system(.caption, design: .rounded))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.green)
+                .fixedSize(horizontal: false, vertical: true)
+                .lineLimit(3)
+            
+            HStack {
+                VStack {
+                    Text("NYK")
+                    Text("111")
+                        .bold()
+                        .font(Font.system(.callout, design: .rounded))
+                }
+                Spacer()
+                
+                Text("Q4 - 4:22")
+                
+                Spacer()
+                
+                VStack {
+                    Text("SAS")
+                    Text("115")
+                        .bold()
+                        .font(Font.system(.callout, design: .rounded))
+                }
+            }
+            .font(Font.system(.footnote, design: .rounded))
         }
         .padding()
     }
